5d2ff20e4a86a4e34880876e120fd1876080e00b
Katherine Saldivar <katherinekaufmann1@gmail.com>
2020-08-14 16:25:21 -0500

    baby shower updates

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,7 +37,7 @@ $("a").mouseup(function(){
 
 // Google Maps Scripts
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+/*google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
     // Basic options for a simple Google Map
@@ -181,4 +181,4 @@ function init() {
         map: map,
         icon: image
     });
-}
+}*/
